bd498c458e2cbfe2346c14dc112d432e4328bfe2
Se Yoon Lee <seyoonlee@stat.tamu.edu>
2022-03-05 21:36:58 -0800

    1

--- R/log_return_four_countries.R
+++
@@ -1,8 +0,0 @@
-#' COVID-19 infection trajectories of 30 countries
-#'
-#' A dataframe contains COVID-19 infection trajectories of 30 countries from Jan 22nd, 2020 to May 14th, 2020.
-#'
-#' @format A data frame with 30 rows and 116 variables:
-#'
-#' @source \url{https://github.com/CSSEGISandData/COVID-19}
-"log_return_four_countries"
