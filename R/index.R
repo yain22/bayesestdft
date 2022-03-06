3272b4a945bc4d743296002defc1a20a9ca86f6f
Se Yoon Lee <seyoonlee@stat.tamu.edu>
2022-03-05 20:38:13 -0800

    1

---
+++ R/index.R
@@ -0,0 +1,7 @@
+#' Log-return rate data for four countries (United States, Japan, Germany, South Korea) spanning from 1 May 2009 to 30 September 2009
+#'
+#' A dataframe contains log-return rate data for four countries (United States, Japan, Germany, South Korea) spanning from 1 May 2009 to 30 September 2009.
+#'
+#' @format A dataframe with 419 rows and 4 variables:
+#'
+"index"
