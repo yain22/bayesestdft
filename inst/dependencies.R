2a1592650fe421f5bce26c6b38b666790c27b250
Se Yoon Lee <seyoonlee@stat.tamu.edu>
2022-03-08 15:43:14 -0800

    1

--- inst/dependencies.R
+++ inst/dependencies.R
@@ -1,17 +1,12 @@
 # No Remotes ----
 # Attachments ----
-#to_install <- c("MASS", "numDeriv")
-#no.packages = length(to_install)
+to_install <- c("MASS", "numDeriv")
+to_install <- c("MASS")
+no.packages = length(to_install)
 
-#for (i in 1:no.packages) {
-#    message(paste("looking for ", to_install[i]))
-#    message(paste("     installing", to_install[i] ))
-#    install.packages(to_install[i])
-#}
-
-install.packages("MASS", repos=c("http://rstudio.org/_packages",
-                                         "http://cran.rstudio.com",dependencies=TRUE))
-
-install.packages("numDeriv", repos=c("http://rstudio.org/_packages",
-                                 "http://cran.rstudio.com",dependencies=TRUE))
+for (i in 1:no.packages) {
+    message(paste("looking for ", to_install[i]))
+    message(paste("     installing", to_install[i] ))
+    install.packages(to_install[i])
+}
 
