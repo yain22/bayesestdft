3b7105bb000678084fd6b36ccba4f0c73ae059bc
Se Yoon Lee <seyoonlee@stat.tamu.edu>
2022-03-08 12:51:51 -0800

    1

--- inst/dependencies.R
+++ inst/dependencies.R
@@ -1,10 +1,12 @@
 # No Remotes ----
 # Attachments ----
-to_install <- c("MASS","numDeriv")
-for (i in to_install) {
-    message(paste("looking for ", i))
-    if (!requireNamespace(i)) {
-        message(paste("     installing", i))
-        install.packages(i)
-    }
+to_install <- c("MASS", "numDeriv")
+no.packages = length(to_install)
+
+for (i in 1:no.packages) {
+    message(paste("looking for ", to_install[i]))
+    message(paste("     installing", to_install[i] ))
+    install.packages(to_install[i])
 }
+
+
