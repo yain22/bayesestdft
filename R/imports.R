85d2cab7af03cf4d31ccea2cca8fdb380fbcc816
Se Yoon Lee <seyoonlee@stat.tamu.edu>
2025-06-03 18:15:41 -0500

    Update imports.R

--- R/imports.R
+++ R/imports.R
@@ -2,6 +2,10 @@
 #' @import pbmcapply
 #' @import foreach
 #' @import doSNOW
-#' @importFrom dplyr mutate summarise count arrange select desc group_by if_else
+#' @importFrom dplyr mutate summarise count arrange select desc group_by if_else full_join
 #' @importFrom kableExtra kbl add_header_above pack_rows group_rows
+#' @importFrom plyr ddply
+#' @importFrom magrittr %>%
+#' @importFrom stats rnorm rgamma rexp rbinom pnorm qf
+#' @importFrom utils txtProgressBar setTxtProgressBar
 NULL
