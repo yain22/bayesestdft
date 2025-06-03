798a24dd23049daa0e9c552d17de453b8b52cefb
Se Yoon Lee <seyoonlee@stat.tamu.edu>
2025-06-03 18:50:35 -0500

    Update winratiosim.R

--- R/winratiosim.R
+++ R/winratiosim.R
@@ -36,7 +36,7 @@
 #' @examples
 #' \dontrun{
 #' power.design_parameters <- list(
-#'   nsim = 100, # nsim = 10000 is the default
+#'   nsim = 10000
 #'   N = 400,
 #'   Randomization.ratio = c(1, 1),
 #'   alpha.JFM = 0, theta.JFM = 1,
