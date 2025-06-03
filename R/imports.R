602d755f526d7a0bd9c2a86597611fa6987b9cd5
Se Yoon Lee <seyoonlee.stat.math@gmail.com>
2025-06-03 17:32:06 -0500

    1

---
+++ R/imports.R
@@ -0,0 +1,7 @@
+#' @import parallel
+#' @import pbmcapply
+#' @import foreach
+#' @import doSNOW
+#' @importFrom dplyr mutate summarise count arrange select desc group_by if_else
+#' @importFrom kableExtra kbl add_header_above pack_rows group_rows
+NULL
