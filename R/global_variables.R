e0b7d328c17ed58c04ba7377f95f637401d97eaa
Se Yoon Lee <seyoonlee.stat.math@gmail.com>
2025-06-03 22:20:04 -0500

    1

--- R/global_variables.R
+++
@@ -1,5 +0,0 @@
-# Declare global variables to avoid CRAN NOTE
-utils::globalVariables(c(
-  ".", "subjid", "z", "summarize", "censor_new", "usubjid1", "score",
-  "treatment1", "treatment2", "usubjid2", "r"
-))
